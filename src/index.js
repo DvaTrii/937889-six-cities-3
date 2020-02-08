f86ef143649b0c5f8286517d1e18c504c8251fdb
Aleks <osinaleksandr1989@gmail.com>
2020-02-08 12:45:02 +0500

    1.3.3 Добавит пропсы в компоненты

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
+const Data = {
+  offersCount: 312
+};
+
 ReactDOM.render(
-    <App/>,
+    <App
+      offersCount={Data.offersCount}/>,
     document.querySelector(`#root`)
 );
