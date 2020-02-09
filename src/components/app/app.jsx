0fdf291ba809e2085fa683b1d7ea18dfa817fcae
Aleks <osinaleksandr1989@gmail.com>
2020-02-09 12:07:05 +0500

    2.1.1 Подключит PropTypes, получит через props и отрендерит карточки обьявлений

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Main from "../main/main.jsx";
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {offersCount} = props;
+  const {offersCount, cards} = props;
 
   return (
-    <Main offersCount={offersCount}/>
+    <Main offersCount={offersCount} cards={cards}
+    />
   );
+};
+
+App.propTypes = {
+  offersCount: PropTypes.number.isRequired,
+  cards: PropTypes.array,
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
 const Data = {
-  offersCount: 312
+  OFFERS_COUNT: 312,
+  CARDS: [`Beautiful & luxurious apartment at great location`, `Wood and stone place`, `Canal View Prinsengracht`,
+    `Nice, cozy, warm big bed apartment`, `Best place to live`],
 };
 
 ReactDOM.render(
     <App
-      offersCount={Data.offersCount}/>,
+      offersCount = {Data.OFFERS_COUNT}
+      cards = {Data.CARDS}/>,
     document.querySelector(`#root`)
 );
