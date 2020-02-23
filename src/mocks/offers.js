4e8167491b793817468f3b94b99ddb32b60b20af
Aleks <osinaleksandr1989@gmail.com>
2020-02-23 19:56:02 +0500

    3.1.1 Создаст моки карточек предложений

---
+++ src/mocks/offers.js
@@ -0,0 +1,39 @@
+export default [
+  {
+    isPremium: true,
+    isBookmark: true,
+    price: 120,
+    img: `img/apartment-01.jpg`,
+    name: `Nice, cozy, warm big bed apartment`,
+    type: `Apartment`,
+    rating: 2,
+  },
+  {
+    isPremium: false,
+    isBookmark: false,
+    price: 110,
+    img: `img/apartment-02.jpg`,
+    name: `Beautiful & luxurious apartment at great location`,
+    type: `Apartment`,
+    rating: 3,
+  },
+  {
+    isPremium: true,
+    isBookmark: false,
+    price: 100,
+    img: `img/apartment-03.jpg`,
+    name: `Canal View Prinsengracht`,
+    type: `Apartment`,
+    rating: 4,
+  },
+  {
+    isPremium: false,
+    isBookmark: true,
+    price: 85,
+    img: `img/room.jpg`,
+    name: `Wood and stone place`,
+    type: `Private room`,
+    rating: 5,
+  }
+];
+
