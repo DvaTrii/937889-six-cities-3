0544c55eacd9a293e386bbe2c61c690647168390
Aleks <osinaleksandr1989@gmail.com>
2020-02-24 10:46:22 +0500

    3.1.2 Создаст и опишет компонент карточка предложения

---
+++ src/components/offer-card/offer-card.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const OfferCard = (props) => {
+
+  const {card, onCardHover} = props;
+
+  const {isPremium, isBookmark, price, image, name, type, rating} = card;
+
+  return (
+    <article className="cities__place-card place-card" onMouseOver={onCardHover}>
+      {isPremium && (
+        <div className="place-card__mark">
+          <span>Premium</span>
+        </div>
+      )}
+      <div className="cities__image-wrapper place-card__image-wrapper">
+        <a href="#">
+          <img className="place-card__image" src={image} width="260" height="200" alt="Place image"/>
+        </a>
+      </div>
+      <div className="place-card__info">
+        <div className="place-card__price-wrapper">
+          <div className="place-card__price">
+            <b className="place-card__price-value">&euro;{price}</b>
+            <span className="place-card__price-text">&#47;&nbsp;night</span>
+          </div>
+          <button className={`place-card__bookmark-button ${isBookmark &&
+          `place-card__bookmark-button--active`} button`} type="button">
+            <svg className="place-card__bookmark-icon" width="18" height="19">
+              <use xlinkHref="#icon-bookmark"></use>
+            </svg>
+            <span className="visually-hidden">To bookmarks</span>
+          </button>
+        </div>
+        <div className="place-card__rating rating">
+          <div className="place-card__stars rating__stars">
+            <span style={{width: `${20 * rating}%`}}></span>
+            <span className="visually-hidden">Rating</span>
+          </div>
+        </div>
+        <h2 className="place-card__name">
+          <a href="#">
+            {name}
+          </a>
+        </h2>
+        <p className="place-card__type">{type}</p>
+      </div>
+    </article>
+  );
+};
+
+OfferCard.propTypes = {
+  card: PropTypes.shape({
+    isPremium: PropTypes.bool.isRequired,
+    isBookmark: PropTypes.bool.isRequired,
+    price: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  }),
+  onCardHover: PropTypes.func.isRequired
+};
+
