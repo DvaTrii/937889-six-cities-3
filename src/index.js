8ff952c3da52934d2c852264f766efbb519717b1
Aleks <osinaleksandr1989@gmail.com>
2020-03-24 14:17:29 +0500

    5.1.4 Подключит redux в index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from "react-dom";
 import cards from "./mocks/offers.js";
 import cities from "./mocks/cities.js";
 
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {reducer} from './reducer';
+
 import App from "./components/app/app.jsx";
+
+const store = createStore(reducer);
 
 const Data = {
   OFFERS_COUNT: 312,
@@ -13,11 +19,13 @@ const Data = {
 };
 
 ReactDOM.render(
-    <App
-      offersCount = {Data.OFFERS_COUNT}
-      cards = {Data.CARDS}
-      cities={Data.CITIES}
-      onCardHover={Data.ON_CARD_HOVER}
-    />,
+    <Provider store={store}>
+      <App
+        offersCount = {Data.OFFERS_COUNT}
+        cards = {Data.CARDS}
+        cities={Data.CITIES}
+        onCardHover={Data.ON_CARD_HOVER}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
